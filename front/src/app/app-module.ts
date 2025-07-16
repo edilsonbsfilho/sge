b9749bc6de137e15cc15fca87bec305cfbd88e92
Edilson Filho <edilsonbsfilho@gmail.com>
2025-07-16 18:25:54 -0300

    fix(editar-evento) corrigindo bug ao editar um evento onde a hora estava sendo cadastrada errada. também foi incluído o campo de inserção de hora do evento

--- front/src/app/app-module.ts
+++ front/src/app/app-module.ts
@@ -1,12 +1,31 @@
-import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { CoreModule } from './core/core.module';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'; 
+import { NativeDateAdapter } from '@angular/material/core';
 import { EventoModule } from './features/evento/evento.module';
+
+registerLocaleData(localePt);
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL', 
+    monthYearA11yLabel: 'MMMM YYYY', 
+  },
+};
+
 @NgModule({
   declarations: [
     App
@@ -21,7 +40,10 @@ import { EventoModule } from './features/evento/evento.module';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection()
+    provideZonelessChangeDetection(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DateAdapter, useClass: NativeDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS }
   ],
   bootstrap: [App]
 })
--- front/src/app/features/evento/components/evento-form/evento-form.component.ts
+++ front/src/app/features/evento/components/evento-form/evento-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EventoService } from '../../../../core/services/evento.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -21,12 +21,14 @@ export class EventoFormComponent implements OnInit {
     private fb: FormBuilder,
     private eventoService: EventoService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private cdr: ChangeDetectorRef,
   ) {  
     this.eventoForm = this.fb.group({
       titulo: ['', [Validators.required, Validators.maxLength(100)]],
       descricao: ['', [Validators.required, Validators.maxLength(1000)]],
-      dataHoraEvento: [new Date(), [Validators.required]],
+      dataHoraEvento: [null, [Validators.required]],
+      horaEvento: ['', [Validators.required, Validators.pattern(/^([01]\d|2[0-3]):([0-5]\d)$/)]],
       local: ['', [Validators.required, Validators.maxLength(200)]]
     });
   }
@@ -37,6 +39,15 @@ export class EventoFormComponent implements OnInit {
         this.isEditMode = true;
         this.eventoId = +params['id'];
         this.loadEvento();
+      } else {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); 
+        this.eventoForm.get('dataHoraEvento')?.setValue(today);
+
+        const now = new Date();
+        const currentHours = now.getHours().toString().padStart(2, '0');
+        const currentMinutes = now.getMinutes().toString().padStart(2, '0');
+        this.eventoForm.get('horaEvento')?.setValue(`${currentHours}:${currentMinutes}`);
       }
     });
   }
@@ -44,25 +55,52 @@ export class EventoFormComponent implements OnInit {
   loadEvento(): void {
     if (this.eventoId) {
       this.isLoading = true;
+      this.cdr.detectChanges();
+
       this.eventoService.getEventoById(this.eventoId).subscribe({
         next: (evento) => {
-          this.eventoForm.patchValue(evento);
+          console.log('Evento carregado do backend:', evento);
+          const loadedDate = new Date(evento.dataHoraEvento); 
+          let dataHoraEventoDate = new Date(loadedDate.getFullYear(), loadedDate.getMonth(), loadedDate.getDate());
+          const hours = loadedDate.getHours().toString().padStart(2, '0');
+          const minutes = loadedDate.getMinutes().toString().padStart(2, '0');
+          let horaEventoString = `${hours}:${minutes}`;
+
+          this.eventoForm.patchValue({
+            titulo: evento.titulo,
+            descricao: evento.descricao,
+            dataHoraEvento: dataHoraEventoDate, 
+            horaEvento: horaEventoString,
+            local: evento.local
+          });
           this.isLoading = false;
         },
         error: () => {
           this.isLoading = false;
+        },
+        complete: () => {
+          this.cdr.detectChanges();
         }
       });
     }
   }
 
   onSubmit(): void {
+    console.log(this.eventoForm.value.dataHoraEvento);
     if (this.eventoForm.valid) {
       this.isLoading = true;
-      const evento: Evento = {
-          ...this.eventoForm.value,
-          eventDateTime: this.eventoForm.value.dataHoraEvento.toISOString()
-        };
+
+      const dataHoraEventoValue = this.eventoForm.value.dataHoraEvento;
+      const horaEventoValue = this.eventoForm.value.horaEvento;
+      
+      const dataHoraEventoISO = this.obterDataISO(dataHoraEventoValue, horaEventoValue);
+
+      const evento:Evento = {
+        titulo: this.eventoForm.value.titulo,
+        descricao: this.eventoForm.value.descricao,
+        dataHoraEvento: dataHoraEventoISO,
+        local: this.eventoForm.value.local,
+      };
       
       const request = this.isEditMode && this.eventoId
         ? this.eventoService.updateEvento({id: this.eventoId, ...evento})
@@ -78,5 +116,34 @@ export class EventoFormComponent implements OnInit {
         }
       });
     }
+  }
+
+  private obterDataISO(dataHoraEventoValue:Date, horaEventoValue:string): string {
+
+    const [hoursStr, minutesStr] = horaEventoValue.split(':');
+    const hours = parseInt(hoursStr, 10);
+    const minutes = parseInt(minutesStr, 10);
+
+    const combinedLocalDate = new Date(
+        dataHoraEventoValue.getFullYear(),
+        dataHoraEventoValue.getMonth(),
+        dataHoraEventoValue.getDate(),
+        hours,
+        minutes,
+        0, 
+        0  
+    );
+
+    const utcDate = new Date(Date.UTC(
+          combinedLocalDate.getFullYear(),
+          combinedLocalDate.getMonth(),
+          combinedLocalDate.getDate(),
+          combinedLocalDate.getHours(),
+          combinedLocalDate.getMinutes(),
+          combinedLocalDate.getSeconds(),
+          combinedLocalDate.getMilliseconds()
+        ));
+        
+    return utcDate.toISOString();
   }
 }
